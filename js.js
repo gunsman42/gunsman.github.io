9c2effe0029adbd53ebdd86930f5885912bddfae
gunsman42 <56871892+gunsman42@users.noreply.github.com>
2020-06-24 22:38:25 +0700

    push
    update1

---
+++ js.js
@@ -0,0 +1,15 @@
+function confirm_alert(node) {
+  return confirm("Coming Soon !");
+}
+
+document.getElementById("text1").innerHTML = "Presents";
+
+function quiz() {
+const version = prompt("Berapa Series Yakuza yang terdapat pada Website ini?");
+if (version === "5" || version ==="lima") {
+    alert("Jawaban Anda Benar!");
+}
+else{
+    alert("Maaf Jawaban Anda salah, jawaban yang benar adalah 5")
+}
+}
